92e83d0ba05e345432a4f9fa72ed2186d8a6a0e8
wookseungim <wookseungim@naver.com>
2020-11-02 11:38:38 +0900

    add function add_lists

--- anything.py
+++ anything.py
@@ -1,3 +1,5 @@
-if __name__ == "__main__":
-    print("hello python, hi ros.")
+from add_lists import add_lists
 
+if __name__ == "__main__":
+    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
+    print(result)
